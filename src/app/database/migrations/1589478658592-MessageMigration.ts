3f542ff03c157e393cca2b7dcaf5655b4aca572c
Lucas Pereira Lopes <lkogici@gmail.com>
2020-05-14 16:45:16 -0300

    NEW: Migration for create table Messages

---
+++ src/app/database/migrations/1589478658592-MessageMigration.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class MessageMigration1589478658592 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "messages",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment"
+                    },
+                    {
+                        name: "text",
+                        type: "varchar",
+                        isNullable: false
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('messages');
+    }
+
+}
