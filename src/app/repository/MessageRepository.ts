24df6fbfd04f381cea93f02c780fbd9251cc9749
Lucas Pereira Lopes <lkogici@gmail.com>
2020-05-14 16:43:29 -0300

    NEW: Created repository

---
+++ src/app/repository/MessageRepository.ts
@@ -0,0 +1,10 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import Message from '../models/Message';
+
+@EntityRepository(Message)
+class MessageRepository extends Repository<Message> {
+  
+}
+
+export default MessageRepository;
